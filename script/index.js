c0f5ec301d12b3e9818d1d32ba4b59f5c93f0533
Olivier Penhoat <openhoat@gmail.com>
2020-01-12 19:46:43 +0100

    review

--- script/index.js
+++ script/index.js
@@ -1,16 +1,45 @@
-const form = document.getElementById("formulaire");
+function main() {
+  const form = document.getElementById("formulaire");
+  const nomElt = document.getElementById("nom");
+  const prenomElt = document.getElementById("prenom");
+  const valeurElt = document.getElementById("message");
 
-const messages = [];
+  const messages = [];
 
-function formHandler(event) {
-  const message = {
-    nom: document.getElementById("nom").value,
-    prenom: document.getElementById("prenom").value,
-    valeur: document.getElementById("message").value
-  };
-  messages.push(message);
-  console.log("messages :", messages);
-  event.preventDefault();
+  function formHandler(event) {
+    const message = {
+      nom: nomElt.value,
+      prenom: prenomElt.value,
+      valeur: valeurElt.value
+    };
+    messages.push(message);
+    console.log("messages :", messages);
+    event.preventDefault();
+  }
+
+  form.onsubmit = formHandler;
 }
 
-form.onsubmit = formHandler;
+/**
+ * Version avancée de la même fonction
+ */
+function mainAlt() {
+  const [form, nomElt, prenomElt, valeurElt] = [
+    "formulaire",
+    "nom",
+    "prenom",
+    "message"
+  ].map(id => document.getElementById(id));
+  const messages = [];
+  form.onsubmit = event => {
+    const [nom, prenom, valeur] = [nomElt, prenomElt, valeurElt].map(
+      elt => elt.value
+    );
+    const message = { nom, prenom, valeur };
+    messages.push(message);
+    console.log("messages :", messages);
+    event.preventDefault();
+  };
+}
+
+window.addEventListener("load", main, false);
